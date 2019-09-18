4d723bd5f021f66d58cd02d3769f307f3bbf1ed7
Carl Hain <crhain@gmail.com>
2019-09-18 11:23:10 -0500

    Add componentDidMount method to UserHeader that runs fetchUser action creator

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -3,6 +3,10 @@ import { connect } from 'react-redux';
 import { fetchUser } from '../actions';
 
 class UserHeader extends Component {
+  componentDidMount() {
+    this.props.fetchUser(this.props.userId);
+  }
+
   render() {
     return <div>User Header</div>;
   }
